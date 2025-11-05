759dcc97ebd5b126d75247eaae7fc5c766ea2e98
Paulo Pinheiro <pinus.sc@gmail.com>
2025-11-05 13:52:31 -0300

    Implemented Section 37 - 164. AOP Examples

---
+++ src/main/java/br/com/paulopinheiro/springmvc/aop/DemoApp.java
@@ -0,0 +1,23 @@
+package br.com.paulopinheiro.springmvc.aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class DemoApp {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext();
+        context.scan("br.com.paulopinheiro.springmvc.aop");
+        context.refresh();
+
+        User user = (User) context.getBean("user");
+
+        System.out.println("==== First Method Demo ====");
+        System.out.println(user.getFirstName());
+
+        System.out.println("==== Seconde Method Demo ====");
+        user.throwException();
+    }
+}
